ed29a6a82f07ea2032919198002dab06d5354edd
Stephen Zweibel <srzweibel@gmail.com>
2014-03-24 12:06:11 -0400

    add workshop

--- _posts/2013-12-31-welcome-to-blogsville.md
+++
@@ -1,6 +0,0 @@
----
-layout: default
-title: Python, CSV, and Regular Expressions Workshop
----
-
-Here are some helpful links:
--- pythonWorkshop/ex2.py
+++
@@ -1,6 +0,0 @@
-print "I will now count my chickens:"
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3
-
-print "Is it true that 3 + 2 < 5 - 7?"
-print 3 + 2 < 5 - 7
