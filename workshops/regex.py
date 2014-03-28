81e46728d4ca70dab474ece27f4e0ae529a6220b
Stephen Zweibel <srzweibel@gmail.com>
2014-03-27 21:48:30 -0400

    workshop

--- workshops/regex.py
+++ workshops/regex.py
@@ -5,9 +5,15 @@ def csv_import(which):
     # Open a CSV
     lines = [line.strip() for line in open(which)]
     for x in lines:
-        match=re.search(r'[a]',x)
-        if match: print x
+        searchObj = re.search( r'(a*) (b.*) (.*)', x)
 
+        if searchObj:
+           print "searchObj.group() :\n", searchObj.group()
+           print "searchObj.group(0) : \n", searchObj.group(0)
+           print "searchObj.group(1) : \n", searchObj.group(1)
+           print "searchObj.group(2) : \n", searchObj.group(2)
+        else:
+           print "Nothing found!!"
 
 csv_import(sys.argv[1])
 
